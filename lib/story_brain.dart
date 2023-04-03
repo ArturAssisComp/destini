c47cfd8bffa982e614fc4a3d3a81f39b4619c420
Artur Assis Alves <60040236+ArturAssisComp@users.noreply.github.com>
2023-04-02 21:32:24 -0300

    Implement the special case when nextStory is called for an end point. (todo 22 and 23)

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -55,6 +55,7 @@ class StoryBrain {
   }
 
   //Methods
+  void restart() => _storyNumber = 0;
   String getStory() => _storyData[_storyNumber].storyTitle;
   String getChoice1() => _storyData[_storyNumber].choice1;
   String getChoice2() => _storyData[_storyNumber].choice2;
@@ -64,18 +65,19 @@ class StoryBrain {
       nextStoryNumber = _storyData[_storyNumber].choice1Child;
     } else if (choiceNumber == 2) {
       nextStoryNumber = _storyData[_storyNumber].choice2Child;
+    } else {
+      //Invalid choiceNumber
+      return;
     }
 
     if (nextStoryNumber != null) {
       _storyNumber = nextStoryNumber;
+    } else {
+      restart();
     }
   }
 }
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
